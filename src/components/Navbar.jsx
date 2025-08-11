ec06eb39c10838315219b452179384d81f46686c
GovindxSharma <govindsharma2839@gmail.com>
2025-08-11 12:15:19 +0530

    png updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import logo from "../assets/aerionstick.PNG"; // update path if needed
+import logo from "../assets/aerionstick.png"; // update path if needed
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
